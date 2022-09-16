b1ff1300464ef44620281da84ef366ab45643e48
Dibyajyoti <parida.dibya2002@gmail.com>
2022-09-16 18:56:51 +0530

    Day 1

---
+++ Day 1/final_task.py
@@ -0,0 +1,6 @@
+# Band Name Generator Program
+
+fav_animal = input("Enter your favorite animal\n")
+city = input("Enter your hometown\n")
+
+print("your band name could be " + city + fav_animal)
---
+++ Day 1/task3.py
@@ -0,0 +1,9 @@
+# Swap two numbers
+
+a = input("Enter first number")
+b = input("Enter second number")
+print("a = " + a + " b = " + b)
+
+# Swapping both numbers
+a, b = b, a
+print("a = " + a + " b = " + b)
